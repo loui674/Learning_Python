02a54a9ce4000e0335a3c83b84405e4477be959b
Luisa <101749657+loui674@users.noreply.github.com>
2022-08-21 10:40:51 -0300

    Exercício 1 em Python
    Um programa simples que informa qual versão do PyCharm estou utilizando

---
+++ Versao_program/main.py
@@ -0,0 +1,8 @@
+import sys
+
+#Programa que informa qual a Versão e suas respectivas informações
+print("Versão do Python")
+print(sys.version)
+
+print("Informação da versão")
+print(sys.version_info)
